b06ebc3a96d3441c84ebf7fb464cb2e523250422
prathyushadhanthala <dhanthala.p@northeastern.edu>
2023-04-11 01:33:25 -0400

    made changes to some of the image processing options

--- IME Project/src/model/ImprovedImageProcessingImpl.java
+++ IME Project/src/model/ImprovedImageProcessingImpl.java
@@ -47,7 +47,7 @@ public class ImprovedImageProcessingImpl extends ImageProcessingModelImpl
       return new double[][]{{0.393, 0.769, 0.189}, {0.349, 0.686, 0.168}, {0.272, 0.534, 0.131}};
     } else if (operation.equals("greyscale")) {
       return new double[][]{{0.2126, 0.7152, 0.0722}, {0.2126, 0.7152, 0.0722},
-                            {0.2126, 0.7152, 0.0722}};
+              {0.2126, 0.7152, 0.0722}};
     }
     return null;
   }
--- IME Project/src/view/ImageProcessingViewImpl.java
+++ IME Project/src/view/ImageProcessingViewImpl.java
@@ -1,5 +1,10 @@
 package view;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,7 +19,7 @@ import model.Color;
 import model.Image;
 import model.Pixel;
 
-public class ImageProcessingViewImpl extends JFrame implements ImageProcessingView{
+public class ImageProcessingViewImpl extends JFrame implements ImageProcessingView {
   private JButton loadButton;
   private JButton saveButton;
   private JButton horizontalFlipButton;
@@ -24,6 +29,7 @@ public class ImageProcessingViewImpl extends JFrame implements ImageProcessingVi
   private JButton sepiaButton;
   private JButton greyscaleButton;
   private JButton ditherButton;
+  private JPanel radioPanel;
   private JButton blurButton;
   private JButton sharpenButton;
   private JButton brightenButton;
@@ -39,6 +45,16 @@ public class ImageProcessingViewImpl extends JFrame implements ImageProcessingVi
   private JPanel loadSaveButtonPanel;
   private List<String> imageParameters;
   private ImageIcon image;
+  private JLabel inputDisplay;
+  private JPanel inputDialogPanel;
+
+  private JRadioButton redRadioButton;
+  private JRadioButton greenRadioButton;
+  private JRadioButton blueRadioButton;
+  private JRadioButton lumaRadioButton;
+  private JRadioButton intensityRadioButton;
+  private JRadioButton valueRadioButton;
+
 
   public ImageProcessingViewImpl() {
     super();
@@ -86,16 +102,17 @@ public class ImageProcessingViewImpl extends JFrame implements ImageProcessingVi
     this.add(imagePanel, BorderLayout.CENTER);
 
     imageLabel = new JLabel(new ImageIcon());
+
     imagePane = new JScrollPane(imageLabel);
 
-    imagePane.setSize(200,200);
+    imagePane.setSize(500, 500);
     imagePane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     imagePane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
     imagePanel.add(imagePane);
 
     histogramLabel = new JLabel(new ImageIcon());
     histogramPane = new JScrollPane(histogramLabel);
-    histogramPane.setSize(200,200);
+    histogramPane.setSize(500, 500);
     histogramPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     histogramPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
     imagePanel.add(histogramPane);
@@ -107,6 +124,7 @@ public class ImageProcessingViewImpl extends JFrame implements ImageProcessingVi
     //input textfield
 //    input = new JTextField(15);
 //    buttonPanel.add(input);
+
 
     brightenButton = new JButton("Brighten");
     brightenButton.setActionCommand("brighten");
@@ -148,7 +166,18 @@ public class ImageProcessingViewImpl extends JFrame implements ImageProcessingVi
     verticalFlipButton.setActionCommand("vertical-flip");
     imageProcessingButtonPanel.add(verticalFlipButton);
 
-    //this.pack();
+    redRadioButton = new JRadioButton("Red-component");
+    redRadioButton.setActionCommand("red-component");
+    greenRadioButton = new JRadioButton("Green-component");
+    greenRadioButton.setActionCommand("green-component");
+    blueRadioButton = new JRadioButton("Blue-component");
+    blueRadioButton.setActionCommand("blue-component");
+    lumaRadioButton = new JRadioButton("Luma-component");
+    lumaRadioButton.setActionCommand("luma-component");
+    intensityRadioButton = new JRadioButton("Intensity-component");
+    intensityRadioButton.setActionCommand("intensity-component");
+    valueRadioButton = new JRadioButton("Value-component");
+    valueRadioButton.setActionCommand("value-component");
   }
 
   @Override
@@ -174,34 +203,110 @@ public class ImageProcessingViewImpl extends JFrame implements ImageProcessingVi
 
   @Override
   public List<String> getParameters(String command) {
-    switch(command){
+    switch (command) {
       case "load":
         String filePath = showLoadImageDialog();
-        if(filePath!=null) {
+        if (filePath != null) {
           return List.of(new String[]{"load", filePath, "image"});
-        }
-        else{
+        } else {
           return null;
         }
       case "save":
       case "brighten":
+        inputDisplay.setText(JOptionPane.showInputDialog("Please enter your username"));
+      case "horizontal-flip":
+        return List.of(new String[]{"horizontal-flip", "image", "image"});
+      case "vertical-flip":
+        return List.of(new String[]{"vertical-flip", "image", "image"});
       case "greyscale":
-        return List.of(new String[]{"greyscale", "luma-component", "image", "image"});
+        String value = showOptionsForGreyScale();
+        return List.of(new String[]{"greyscale", value, "image", "image"});
       case "rgb-split":
+        return List.of(new String[]{"rgb-split", "image", "image", "image", "image"});
       case "rgb-combine":
+        return List.of(new String[]{"rgb-combine", "image", "image", "image", "image"});
+      case "sepia":
+        return List.of(new String[]{"sepia", "image", "image"});
+      case "dither":
+        return List.of(new String[]{"dither", "image", "image"});
+      case "blur":
+        return List.of(new String[]{"blur", "image", "image"});
+      case "sharpen":
+        return List.of(new String[]{"sharpen", "image", "image"});
     }
     return imageParameters;
   }
 
+  private String showOptionsForGreyScale() {
+    radioPanel = new JPanel();
+    radioPanel.setLayout(new BoxLayout(radioPanel, BoxLayout.Y_AXIS));
+    JLabel label = new JLabel("Select an option:");
+    radioPanel.add(label);
+    radioPanel.add(redRadioButton);
+    radioPanel.add(greenRadioButton);
+    radioPanel.add(blueRadioButton);
+    radioPanel.add(lumaRadioButton);
+    radioPanel.add(intensityRadioButton);
+    radioPanel.add(valueRadioButton);
+    ButtonGroup greyscaleOptions = new ButtonGroup();
+    greyscaleOptions.add(redRadioButton);
+    greyscaleOptions.add(greenRadioButton);
+    greyscaleOptions.add(blueRadioButton);
+    greyscaleOptions.add(lumaRadioButton);
+    greyscaleOptions.add(intensityRadioButton);
+    greyscaleOptions.add(valueRadioButton);
+    JOptionPane optionPaneRB = new JOptionPane();
+    JDialog dialog = null;
+    optionPaneRB.setMessage("Please select an option");
+    optionPaneRB.setMessageType(JOptionPane.PLAIN_MESSAGE);
+    optionPaneRB.setOptionType(JOptionPane.DEFAULT_OPTION);
+    optionPaneRB.add(radioPanel, 1);
+    dialog = optionPaneRB.createDialog(this, "Greyscale on component");
+    dialog.setVisible(true);
+    if (optionPaneRB.getValue() == null) {
+      return null;
+    }
+    return greyscaleOptions.getSelection().getActionCommand();
+  }
+
   @Override
   public void displayCurrentImage(List<Image> m) {
-    if(m.size()==1){
-      image = new ImageIcon(getImageToDisplay(m.get(0)));
+    if (m.size() == 1) {
+      BufferedImage sourceImage = getImageToDisplay(m.get(0));
+      int[] redHistogram = new int[256];
+      int[] greenHistogram = new int[256];
+      int[] blueHistogram = new int[256];
+      int[] intensityHistogram = new int[256];
+      for (int y = 0; y < sourceImage.getHeight(); y++) {
+        for (int x = 0; x < sourceImage.getWidth(); x++) {
+          Color c = new Color(sourceImage.getRGB(x, y));
+          redHistogram[c.getRed()]++;
+          greenHistogram[c.getGreen()]++;
+          blueHistogram[c.getBlue()]++;
+          int intensity = (int) (0.299 * c.getRed() + 0.587 * c.getGreen() + 0.114 * c.getBlue());
+          intensityHistogram[intensity]++;
+        }
+      }
+      DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+      for (int i = 0; i < 256; i++) {
+        dataset.addValue(redHistogram[i], "Red", Integer.toString(i));
+        dataset.addValue(greenHistogram[i], "Green", Integer.toString(i));
+        dataset.addValue(blueHistogram[i], "Blue", Integer.toString(i));
+        dataset.addValue(intensityHistogram[i], "Intensity", Integer.toString(i));
+      }
+      JFreeChart chart = ChartFactory.createLineChart("Histogram", "Value",
+              "Frequency", dataset, PlotOrientation.VERTICAL, true,
+              true, false);
+      image = new ImageIcon(sourceImage);
       imageLabel.setIcon(image);
+      Dimension size = imageLabel.getPreferredSize();
+      size.width = 450;
+      size.height = 450;
+      imageLabel.setPreferredSize(size);
+      ImageIcon histogramIcon = new ImageIcon(chart.createBufferedImage(450, 450));
+      histogramLabel.setIcon(histogramIcon);
       validate();
-      //imageLabel.revalidate();
-    }
-    else{
+    } else {
       rgbSplitSaveDialog(m);
     }
   }
@@ -211,7 +316,7 @@ public class ImageProcessingViewImpl extends JFrame implements ImageProcessingVi
 
   }
 
-  private BufferedImage getImageToDisplay(Image m){
+  private BufferedImage getImageToDisplay(Image m) {
     BufferedImage image = new BufferedImage(m.getWidth(), m.getHeight(),
             BufferedImage.TYPE_INT_RGB);
     Pixel[][] listOfPixels = m.getPixels();
@@ -231,33 +336,36 @@ public class ImageProcessingViewImpl extends JFrame implements ImageProcessingVi
     return image;
   }
 
-  private void rgbSplitSaveDialog(List<Image> m){
+  private void rgbSplitSaveDialog(List<Image> m) {
     JDialog rgbSplitSave = new JDialog();
   }
 
-  private String showLoadImageDialog(){
+  private String showLoadImageDialog() {
     JFileChooser fc = new JFileChooser(".");
     int returnVal = fc.showOpenDialog(this);
     if (returnVal == JFileChooser.APPROVE_OPTION) {
       File file = fc.getSelectedFile();
       //This is where a real application would open the file.
-      System.out.println("Opening: " + file.getName() + "." );
+      System.out.println("Opening: " + file.getName() + ".");
       return file.getPath();
     } else {
       System.out.println("Open command cancelled by user.");
       return null;
     }
   }
-  private void showSaveImageDialog(){
+
+  private void showSaveImageDialog() {
     JFileChooser fc = new JFileChooser(".");
     int returnVal = fc.showSaveDialog(this);
     if (returnVal == JFileChooser.APPROVE_OPTION) {
       File file = fc.getSelectedFile();
       //This is where a real application would save the file.
-      System.out.println("Saving: " + file.getName() + "." );
+      System.out.println("Saving: " + file.getName() + ".");
 
     } else {
       System.out.println("Save command cancelled by user.");
     }
   }
+
+
 }
